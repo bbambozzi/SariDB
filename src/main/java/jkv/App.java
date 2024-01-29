094bf47e332d021724d5b40ffcc5554ee7d636a6
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-01-29 02:21:34 -0300

    First commit!

---
+++ src/main/java/jkv/App.java
@@ -0,0 +1,11 @@
+package jkv;
+
+/**
+ * @author Bautista Bambozzi - 2024
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
