17dffd86b7bee523deaf0c13d21c80037701d92e
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-02-02 00:48:21 -0300

    Improve client example's logic!

--- src/main/java/client/ClientExample.java
+++ src/main/java/client/ClientExample.java
@@ -3,11 +3,13 @@ package client;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,34 +18,37 @@ public record ClientExample() {
     private static final Logger logger = Logger.getLogger(ClientExample.class.getName());
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        String serverAddress = "localhost";
+        String serverAddress = args.length >= 1 ? args[0] : "localhost";
         int port = 1338;
-        int amount = 300;
-        var executor = Executors.newVirtualThreadPerTaskExecutor();
-
+        int amount = 100;
+        AtomicInteger queries = new AtomicInteger();
         System.out.println("Starting " + amount + " sockets");
-
         List<Thread> threads = new ArrayList<>();
+        Instant start = Instant.now();
         for (int i = 0; i < amount; i++) {
             threads.add(Thread.ofVirtual().start(() -> {
                 try (Socket clientSocket = new Socket(serverAddress, port)) {
-                    String dataToSend;
-                    int rand = random.nextInt(0, 2);
                     OutputStream outputStream = clientSocket.getOutputStream();
                     InputStream inputStream = clientSocket.getInputStream();
-                    if (rand == 0) {
-                        dataToSend = "SET " + random.nextInt(1000) + 10_000 + " " + random.nextInt(100_000);
-                    } else {
-                        dataToSend = "GET " + random.nextInt(89_000) + 10_000 + " " + random.nextInt(100_000);
+                    String dataToSend;
+                    for (int j = 0; j < 10; j++) {
+                        queries.getAndAdd(1);
+
+                        int rand = random.nextInt(0, 2);
+                        if (rand == 0) {
+                            dataToSend = "SET " + random.nextInt(1000) + 10_000 + " " + random.nextInt(100_000);
+                        } else {
+                            dataToSend = "GET " + random.nextInt(1000) + 10_000 + " " + random.nextInt(100_000);
+                        }
+
+                        outputStream.write(dataToSend.getBytes());
+                        outputStream.flush();
+
+                        // Receive and print the response
+                        byte[] responseBuffer = new byte[1024];
+                        int bytesRead = inputStream.read(responseBuffer);
+                        // System.out.println("Response = " + new String(responseBuffer));
                     }
-
-                    outputStream.write(dataToSend.getBytes());
-                    outputStream.flush();
-
-                    // Receive and print the response
-                    byte[] responseBuffer = new byte[1024];
-                    int bytesRead = inputStream.read(responseBuffer);
-                    // System.out.println("Response = " + new String(responseBuffer));
                 } catch (Exception ex) {
                     logger.log(Level.INFO, "FAILURE");
                     logger.log(Level.INFO, ex.getMessage());
@@ -59,9 +64,10 @@ public record ClientExample() {
                     }
                 }
         );
-        /* TODO wait until all threads are finished! */
+        Instant end = Instant.now();
         logger.log(Level.INFO, "Finished " + amount + " connections!");
-        executor.close();
+        logger.log(Level.INFO, "Total queries=" + queries);
+        logger.log(Level.INFO, "Time Taken=" + Duration.between(start, end).toMillis() + " milliseconds.");
     }
 
 }
