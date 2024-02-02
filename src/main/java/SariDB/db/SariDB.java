0f08d69b7b61e0577b225ece27250acc5bea0aeb
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-02-02 18:45:39 -0300

    Improve the JavaDoc documentation!

--- src/main/java/SariDB/db/SariDB.java
+++ src/main/java/SariDB/db/SariDB.java
@@ -5,7 +5,13 @@ import SariDB.handler.ClientHandler;
 /**
  * @author Bautista Bambozzi
  * @version 0.1
- * @since 2023
+ * @since 2024
+ * <h3>A lightweight, concurrent key-value database that can be embedded or deployed standalone.</h3>
+ * <p>Can be instantiated via the {@code SariDB.builder()} method.</p>
+ * Represents a SariDB instance that can be configured using the Builder pattern.
+ * This class allows creating a SariDB object with specified properties such as
+ * the file path and whether it is embedded.
+ * @see SariDB.Builder
  */
 public class SariDB {
     private final boolean isEmbedded;
@@ -16,25 +22,48 @@ public class SariDB {
         this.isEmbedded = builder.isEmbedded;
     }
 
-     static void main(String[] args) {
-        int portNumber = args.length >= 1 ? Integer.parseInt(args[0]) : 1338;
-        ClientHandler clientHandler = new ClientHandler(portNumber);
-        clientHandler.handleClients();
+    /**
+     * Creates a new instance of the SariDB Builder.
+     *
+     * @return A new instance of the SariDB Builder.
+     */
+    public static Builder builder() {
+        return new Builder();
     }
 
+    /**
+     * Builder class for configuring and creating a SariDB instance.
+     */
     public static class Builder {
         private boolean isEmbedded;
         private String filePath;
 
+        /**
+         * Builds and returns the SariDB instance with the configured properties.
+         *
+         * @return The configured SariDB instance.
+         */
         public SariDB build() {
             return new SariDB(this);
         }
 
+        /**
+         * Sets the file path for the SariDB instance being built.
+         *
+         * @param filePath The file path to set.
+         * @return The Builder instance for method chaining.
+         */
         public Builder filePath(String filePath) {
             this.filePath = filePath;
             return this;
         }
 
+        /**
+         * Sets whether the SariDB instance being built is embedded.
+         *
+         * @param isEmbedded True if the SariDB is embedded, false otherwise.
+         * @return The Builder instance for method chaining.
+         */
         public Builder isEmbedded(boolean isEmbedded) {
             this.isEmbedded = isEmbedded;
             return this;
