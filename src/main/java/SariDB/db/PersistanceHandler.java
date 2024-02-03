96c9855adce4bdbfad5f0b6781a978432d278ed1
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-02-03 09:58:58 -0300

    Fixed the ParquetWriter!

--- src/main/java/SariDB/db/PersistanceHandler.java
+++ src/main/java/SariDB/db/PersistanceHandler.java
@@ -19,40 +19,13 @@ import java.util.Map;
 public record PersistanceHandler(Path path) {
 
     private static final Schema SCHEMA = new Schema.Parser().parse("{\"type\":\"record\",\"name\":\"KeyValue\",\"fields\":[{\"name\":\"key\",\"type\":\"string\"},{\"name\":\"value\",\"type\":\"string\"}]}");
-    private static final Configuration CONF =  new Configuration();
-
-    public void writeToFile(Map<String, String> map) throws IOException {
-        // Define the Parquet schema
-        MessageType schema = getMessageType();
-
-        // Configure ParquetWriter
-
-        try (ParquetWriter<Void> writer = getParquetWriter()) {
-            // Write data to the Parquet file
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                writer.write(getRecord(entry.getKey(), entry.getValue())); // required Void got GenericRecord
-            }
-        }
-    }
+    private static final Configuration CONF = new Configuration();
 
     private static MessageType getMessageType() {
         // Define the schema for the Parquet file
         return MessageTypeParser.parseMessageType(
                 "message KeyValue { required binary key (UTF8); required binary value (UTF8); }"
         );
-    }
-
-    private ParquetWriter<Void> getParquetWriter() throws IOException {
-        // Specify the compression codec
-        CompressionCodecName codec = CompressionCodecName.SNAPPY;
-
-        // Create a ParquetWriter
-        OutputFile outputFile = HadoopOutputFile.fromPath(path, CONF);
-        return AvroParquetWriter.<Void>builder(outputFile)
-                .withConf(CONF)
-                .withSchema(SCHEMA) //
-                .withCompressionCodec(codec)
-                .build();
     }
 
     private static GenericRecord getRecord(String key, String value) {
@@ -67,6 +40,33 @@ public record PersistanceHandler(Path path) {
     private static String getAvroSchemaString() {
         // Define the Avro schema
         return "{\"type\":\"record\",\"name\":\"KeyValue\",\"fields\":[{\"name\":\"key\",\"type\":\"string\"},{\"name\":\"value\",\"type\":\"string\"}]}";
+    }
+
+    public void writeToFile(Map<String, String> map) throws IOException {
+        // Define the Parquet schema
+        MessageType schema = getMessageType();
+
+        // Configure ParquetWriter
+
+        try (ParquetWriter<GenericRecord> writer = getParquetWriter()) {
+            // Write data to the Parquet file
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                writer.write(getRecord(entry.getKey(), entry.getValue())); // required Void got GenericRecord
+            }
+        }
+    }
+
+    private ParquetWriter<GenericRecord> getParquetWriter() throws IOException {
+        // Specify the compression codec
+        CompressionCodecName codec = CompressionCodecName.SNAPPY;
+
+        // Create a ParquetWriter
+        OutputFile outputFile = HadoopOutputFile.fromPath(path, CONF);
+        return AvroParquetWriter.<GenericRecord>builder(outputFile)
+                .withConf(CONF)
+                .withSchema(SCHEMA) //
+                .withCompressionCodec(codec)
+                .build();
     }
 
     public void main(String[] args) {
