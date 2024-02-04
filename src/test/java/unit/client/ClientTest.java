eb6d760faa3385b54d43d624f524d16d9c1a18bb
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-02-04 19:14:09 -0300

    Started unit tests for the standalone SariDB client

--- src/test/java/unit/client/ClientTest.java
+++ src/test/java/unit/client/ClientTest.java
@@ -1,4 +1,21 @@
 package unit.client;
 
+import client.SariDBClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+
+import java.net.Socket;
+
 public class ClientTest {
+    @Nested
+    public class ClientQueriesStandaloneSariDB {
+        SariDBClient client;
+
+        @BeforeEach
+        public void setUp() throws Exception {
+            try (Socket socket = new Socket("localhost", 1338)) {
+                this.client = new SariDBClient(socket.getInputStream(), socket.getOutputStream());
+            }
+        }
+    }
 }
