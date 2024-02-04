21a8c3fcd37a3145955144804d5673fcdd8a2a9f
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-02-04 19:38:10 -0300

    Added new @Displayname tags to every single SariDB standalone integration test

--- src/test/java/integration/SariDBAndClient/SariDBStandaloneAndClient.java
+++ src/test/java/integration/SariDBAndClient/SariDBStandaloneAndClient.java
@@ -2,6 +2,7 @@ package integration.SariDBAndClient;
 
 import client.SariDBClient;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -11,22 +12,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SariDBStandaloneAndClient {
     @Nested
+    @DisplayName("When a SariDB client connects to a standalone SariDB instance")
     public class ClientQueriesStandaloneSariDB {
         SariDBClient client;
         private Socket socket;
 
         @BeforeEach
+        @DisplayName("Sets up correctly")
         public void setUp() throws Exception {
             this.socket = new Socket("localhost", 1338);
             this.client = new SariDBClient(socket.getInputStream(), socket.getOutputStream());
         }
 
         @Test
+        @DisplayName("SET commands are registered correctly")
         public void shouldSetCorrectly() throws Exception {
             String answer = client.sendSetRequest("one", "one");
             assertEquals("OK", answer);
         }
 
+        @DisplayName("Non-existing values return 'null'")
         @Test
         public void shouldGetNullValuesCorrectly() throws Exception {
             String answer = client.sendGetRequest("IDONTEXIST");
@@ -35,6 +40,7 @@ public class SariDBStandaloneAndClient {
 
 
         @Test
+        @DisplayName("Setting and then getting a value will yield the correct result")
         public void shouldSetAndGetValuesCorrectly() throws Exception {
             String setValue = "testingValue";
             String setKey = "testingkey";
