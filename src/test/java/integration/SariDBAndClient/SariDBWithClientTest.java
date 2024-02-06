04064c1a61de4051bf6404dd18c82c8d90ada6b3
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-02-06 08:26:11 -0300

    Disabled some integration tests that have no mocks

--- src/test/java/integration/SariDBAndClient/SariDBWithClientTest.java
+++ src/test/java/integration/SariDBAndClient/SariDBWithClientTest.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisabledIfSystemProperty(named = "ci.environment", matches = "true")
+@Disabled
 public class SariDBWithClientTest {
     private static AtomicInteger count = new AtomicInteger(5759);
     @Nested
